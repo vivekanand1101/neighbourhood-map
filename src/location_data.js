4149eaf74510654feeaf5ddc5ca1e468716fb13e
Vivek Anand <vivekanand1101@gmail.com>
2018-12-03 22:22:37 +0530

    Add a locationk, remove default create react app html
    Signed-off-by: Vivek Anand <vivekanand1101@gmail.com>

---
+++ src/location_data.js
@@ -0,0 +1,8 @@
+export const locations = [
+    {
+        title: 'Rodeo',
+        position: {lat: 28.633045, lng: 77.217901},
+        address: '12-A, Rajiv Chowk, Block A, Connaught Place, New Delhi, Delhi 110001, India',
+        placeId: 'ChIJwwKF2zf9DDkRkVJgARVHV4I'
+    },
+];
